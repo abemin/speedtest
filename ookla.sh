e111d26da1b6ebb30bb141c2d0d07e4de6004bce
abemin <muhaimifatihi@hotmail.com>
2021-08-24 15:13:09 +0800

    Update ookla.sh

--- ookla.sh
+++ ookla.sh
@@ -2,4 +2,4 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
 pkill -f speedtest
-/usr/bin/python3 /home/administrator/speedtest_to_influx.py
+/usr/bin/python3 /home/administrator/speedtest_to_influx.py #change user if different
